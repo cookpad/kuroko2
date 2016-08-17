0fc14da5efa23a14369a4daf5a7c22f9c241863a
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-08-17 18:08:29 +0900

    Copy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,55 @@
 Kuroko2::Engine.routes.draw do
+  resources :job_definitions, path: 'definitions' do
+    get 'page/:page', action: :index, on: :collection, as: 'paged'
+    resources :stars, only: %w(create destroy)
+    resources :job_instances, path: 'instances', only: %w(index create show destroy) do
+      get 'naked', action: :show, on: :member, mode: :naked
+      get 'page/:page', action: :index, on: :collection, as: 'paged'
+      delete 'force_destroy', action: :force_destroy, on: :member
+      resources :tokens, only: %w(index update)
+      resources :executions, only: %w(destroy)
+      resources :logs, only: %w(index)
+      resources :execution_logs, only: %w(index)
+    end
+    resources :job_schedules, path: 'schedules', only: %w(index create destroy)
+    resources :job_suspend_schedules, path: 'suspend_schedules', only: %w(index create destroy)
+    resources :job_definition_stats, path: 'stats', only: %w(index) do
+      get :memory, action: :memory, on: :collection, defaults: { format: 'json' }
+      get :execution_time, action: :execution_time, on: :collection, defaults: { format: 'json' }
+    end
+  end
+  resources :users do
+    get 'page/:page', action: :index, on: :collection, as: 'paged'
+  end
+
+  resources :workers, only: :index
+  resources :job_instances, path: 'instances', only: %w() do
+    get :working, action: :working, on: :collection
+  end
+
+  resources :job_timelines, only: :index do
+    get :dataset, action: :dataset, on: :collection, defaults: { format: 'json' }
+  end
+
+  get '/sign_in', to: 'sessions#new', as: 'sign_in'
+  delete '/sign_out', to: 'sessions#destroy', as: 'sign_out'
+
+  get '/auth/:provider', as: :auth, to: lambda { |_env| [500, {}, 'Never called'] }
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  get 'hello/revision'
+
+  root 'dashboard#index'
+  get '/osd' => 'dashboard#osd', as: :osd
+
+  scope :v1, module: 'api', as: 'api' do
+    resources :job_definitions, path: 'definitions', only: [] do
+      resources :job_instances, path: 'instances', only: [:show, :create]
+    end
+
+    namespace :stats do
+      get :instance
+      get :waiting_execution
+    end
+  end
 end
