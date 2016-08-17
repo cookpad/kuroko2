a07bcff1c0971a9121e27b451323b924504529b2
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-08-17 13:55:31 +0900

    Add requiring gems to before_configuration

--- lib/kuroko2/engine.rb
+++ lib/kuroko2/engine.rb
@@ -1,5 +1,10 @@
 module Kuroko2
   class Engine < ::Rails::Engine
     isolate_namespace Kuroko2
+
+    config.before_configuration do
+      require 'kaminari'
+      require 'chrono'
+    end
   end
 end
