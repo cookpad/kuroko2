0d02cbb5ea51734772b34e312b48d9c11e6627f1
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-08-25 17:32:39 +0900

    Copy command specs

---
+++ spec/command/monitor_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+module Command
+  describe Monitor do
+    let(:pid) { 32769 } # max int + 1
+    let(:hostname) { 'rspec' }
+
+    let(:definition) { create(:job_definition) }
+    let(:instance) { create(:job_instance, job_definition: definition) }
+    let(:token) { create(:token, job_definition: definition, job_instance: instance) }
+    let(:worker) { create(:worker, hostname: hostname) }
+
+    describe '#execute' do
+      let(:sent_mail) { ActionMailer::Base.deliveries.last }
+
+      context 'not running process' do
+        let!(:execution) { create(:execution, token: token, worker: worker, started_at: Time.now, finished_at: nil, pid: pid) }
+        let(:monitor) { Command::Monitor.new(hostname: hostname, worker_id: 1) }
+
+        subject! { 15.times { monitor.execute } }
+
+        it 'sends a notification mail' do
+          execution.reload
+
+          expect(monitor.counter_size).to be_zero
+          expect(sent_mail).not_to be_nil
+          expect(sent_mail.subject).to eq '[CRITICAL] Process is not running on kuroko'
+          expect(execution.mailed_at).not_to be_nil
+        end
+      end
+
+      context 'not assigned process' do
+        let!(:execution) { create(:execution, token: token, worker: worker, started_at: 2.minutes.ago, finished_at: nil, pid: nil) }
+        subject! { Command::Monitor.new(hostname: hostname, worker_id: 1).execute }
+
+        it 'sends a notification mail' do
+          execution.reload
+
+          expect(sent_mail.subject).to eq '[CRITICAL] Process is not assigned to any job-executor'
+          expect(execution.mailed_at).not_to be_nil
+        end
+      end
+    end
+
+    describe 'memory consumption monitoring' do
+      let!(:execution) { create(:execution, token: token, worker: worker, started_at: Time.now, finished_at: nil, pid: pid) }
+      let(:monitor) { Command::Monitor.new(hostname: hostname, worker_id: 1) }
+      before do
+        allow(monitor).to receive(:check_process_absence).and_return(true)
+        allow(MemorySampler).to receive(:get_by_pgid).and_return(1)
+      end
+
+      it 'logs memory consumption' do
+        expect {
+          monitor.execute
+        }.to change {
+          execution.job_definition.memory_expectancy.memory_consumption_logs.count
+        }.from(0).to(1)
+
+        log = execution.job_definition.memory_expectancy.memory_consumption_logs.first
+        expect(log.value).to be_kind_of(Integer)
+        expect(log.value).not_to eq(0)
+        expect(log.job_instance_id).to eq(instance.id)
+      end
+    end
+  end
+end
---
+++ spec/command/shell_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+module Command
+  describe Shell do
+
+    describe '#execute' do
+      subject { Shell.new(hostname: 'rspec', worker: worker).execute }
+
+      let(:job_definition) { create(:job_definition) }
+      let(:job_instance) { create(:job_instance, job_definition: job_definition) }
+      let(:token) { create(:token, job_instance: job_instance, job_definition: job_definition) }
+      let!(:execution) { create(:execution, shell: shell, context: { 'ENV' => { 'NAME' => "kuroko" } }, started_at: nil, finished_at: nil, token: token) }
+      let(:shell) { 'ruby -e "true"' }
+
+      context 'when available worker exists' do
+        let(:worker) { create(:worker) }
+
+        context 'successfully' do
+          let(:shell) { 'ruby -e "puts ENV[\'NAME\'] + \\"\\\x99\\""' }
+
+          it do
+            is_expected.to eq execution
+
+            expect(subject.started_at).not_to be_nil
+            expect(subject.finished_at).not_to be_nil
+            expect(subject).to be_success
+          end
+        end
+
+        context 'failure' do
+          let(:shell) { 'wrong_script' }
+
+          it do
+            is_expected.to eq execution
+
+            expect(subject.started_at).not_to be_nil
+            expect(subject.finished_at).not_to be_nil
+            expect(subject).not_to be_success
+          end
+        end
+
+        context 'with 4byte utf-8 charactor' do
+          let(:shell) { 'ruby -e "puts ENV[\'NAME\'] + \\"\xF0\x9F\x98\x81\\""' }
+
+          it do
+            is_expected.to eq execution
+
+            expect(subject.started_at).not_to be_nil
+            expect(subject.finished_at).not_to be_nil
+            expect(subject).to be_success
+          end
+        end
+      end
+
+      context 'when all workers are busy' do
+        let(:worker) { create(:worker, execution_id: execution.id) }
+
+        it 'skips execution' do
+          is_expected.to be_nil
+          execution.reload
+          expect(execution.started_at).to be_nil
+        end
+      end
+
+      describe 'memory expectancy calculation' do
+        let(:worker) { create(:worker) }
+        let(:memory_expectancy) { execution.job_definition.memory_expectancy }
+
+        context 'when memory consumption logs exist' do
+          before do
+            (1..10).each {|i| job_instance.create_memory_consumption_log!(value: i) }
+          end
+
+          it 'calculates memory expectancy of related job definition' do
+            expect { subject }.to change { memory_expectancy.reload.expected_value }
+          end
+        end
+
+        context 'when no memory consumption logs exist' do
+          it 'does not calculates memory expectancy of related job definition' do
+            expect { subject }.not_to change { memory_expectancy.reload.expected_value }
+          end
+        end
+      end
+    end
+  end
+end
