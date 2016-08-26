fbb705d7f9c0ff3cd34cd009ddbd28ad25e9651d
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-08-26 17:29:33 +0900

    Add lib/servers

---
+++ lib/servers/command-executor.rb
@@ -0,0 +1,20 @@
+require 'kuroko2'
+
+if Rails.env.development?
+  ActionMailer::Base.logger = Kuroko2.logger
+end
+
+options = {
+  worker_type: 'process',
+  workers:     Command::Executor.num_workers,
+  daemonize:   Rails.env.production?,
+  log:         Rails.env.production? ? Rails.root.join("log/command-executor.log").to_s : $stdout,
+  log_level:   Rails.env.production? ? :info : :debug,
+  pid_path:    Rails.root.join('tmp/pids/command-executor.pid').to_s,
+  supervisor:  Rails.env.production?,
+
+  worker_graceful_kill_timeout: -1,
+}
+
+server = ServerEngine.create(nil, Command::Executor, options)
+server.run
---
+++ lib/servers/job-scheduler.rb
@@ -0,0 +1,17 @@
+require 'kuroko2'
+
+if Rails.env.development?
+  ActionMailer::Base.logger = Kuroko2.logger
+end
+
+options = {
+  worker_type: 'embedded',
+  daemonize:   Rails.env.production?,
+  log:         Rails.env.production? ? Rails.root.join('log/job-scheduler.log').to_s : $stdout,
+  log_level:   Rails.env.production? ? :info : :debug,
+  pid_path:    Rails.root.join('tmp/pids/job-scheduler.pid').to_s,
+  supervisor:  Rails.env.production?,
+}
+
+server = ServerEngine.create(nil, Workflow::Scheduler, options)
+server.run
