d8a3da323674c02c7995263e56593d8176b7eb72
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-08-29 14:27:05 +0900

    fixup! Add custom task loading spec

--- spec/settings_spec.rb
+++ spec/settings_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 module Kuroko2
   describe 'Check if settings is loaded' do
     it 'loads custom tasks' do
-      expect(Workflow::Node::TASK_REGISTORY.has_key?(:custom_task1)).to eq(true)
-      expect(Workflow::ScriptParser.new('custom_task1:').parse.class).to eq(Workflow::Node)
+      expect(Workflow::Node::TASK_REGISTORY).to have_key(:custom_task1)
+      expect(Workflow::ScriptParser.new('custom_task1:').parse).to be_a(Workflow::Node)
     end
   end
 end
