e89e336d8bd92914aae228e10ff253f95b983360
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-08-31 11:54:00 +0900

    Fix assets path

--- app/assets/javascripts/kuroko2/application.js
+++ app/assets/javascripts/kuroko2/application.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= stub instance_linker
+//= stub kuroko2/instance_linker
 //= require_tree
 //= require moment
 //= require bootstrap-sortable
