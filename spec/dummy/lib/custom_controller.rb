afe8899d20eee95c5048ce10213266de4ba3bfb4
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-09-01 16:34:35 +0900

    Fix method name

--- spec/dummy/lib/custom_controller.rb
+++ spec/dummy/lib/custom_controller.rb
@@ -1,11 +1,14 @@
 module CustomController
   extend ActiveSupport::Concern
   included do
-    before_action :hoge
+    before_action :additional_before_action
   end
 
   private
 
-  def hoge
+  def additional_before_action
+    if signed_in?
+      # do something
+    end
   end
 end
