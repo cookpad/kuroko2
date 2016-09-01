c7e091cff91847bc9930dd9beb99ee200a1c9405
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-09-01 15:55:07 +0900

    Require app_authentication.google_oauth2 configuration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,5 @@
-config = Kuroko2.config.app_authentication.try!(:google_oauth2)
-if config.present?
-  require 'omniauth-google-oauth2'
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, config.client_id, config.client_secret, options
-  end
+require 'omniauth-google-oauth2'
+Rails.application.config.middleware.use OmniAuth::Builder do
+  config = Kuroko2.config.app_authentication.google_oauth2
+  provider :google_oauth2, config.client_id, config.client_secret, config.options
 end
