cf3fcd48f38f40d895dc3003be75eb7fc6d7aba7
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-09-02 11:44:31 +0900

    Move dummy custom tasks

---
+++ spec/dummy/lib/kuroko2/workflow/task/custom_task1.rb
@@ -0,0 +1,13 @@
+module Kuroko2
+  module Workflow
+    module Task
+      class CustomTask1 < Base
+
+        def execute
+          Kuroko2.logger.info("(token #{token.uuid}) Custom Task1")
+          :next
+        end
+      end
+    end
+  end
+end
