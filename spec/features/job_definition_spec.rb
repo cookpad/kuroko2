a7df46958749284fe787dae8834942428fca8aaa
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-09-13 14:41:08 +0900

    Add job_definition feature spec

---
+++ spec/features/job_definition_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "JobDefinition", type: :feature do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in(user)
+  end
+
+  it 'creates/edit/destroy job_definition', js: true do
+    visit kuroko2.root_path
+
+    within '.sidebar-menu' do
+      click_on 'Create New'
+    end
+
+    fill_in 'job_definition_name', with: 'test1'
+    fill_in 'job_definition_description', with: 'description'
+    fill_in 'job_definition_script', with: 'noop:'
+    fill_in 'job_definition_text_tags', with: 'tag_1, common_tag'
+
+    click_on 'Create Job definition'
+
+    expect(page).to have_content('Job Definition Details')
+    expect(page).to have_content('test1')
+
+    click_on 'Edit Job definition'
+
+    fill_in 'job_definition_name', with: 'test1-edit'
+    click_on 'Update Job definition'
+
+    expect(page).to have_content('Job Definition Details')
+    expect(page).to have_content('test1-edit')
+
+    click_on 'Destroy Job definition'
+    expect(page).to have_content('All Job Definitions')
+  end
+end
