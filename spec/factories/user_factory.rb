8d30e4b7dfb69a3b48fad9abb433d45c543798d2
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-09-13 18:03:07 +0900

    Add randomize to user factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :user, class: Kuroko2::User do
-    sequence(:uid) { |n| "#{n}" }
+    sequence(:uid) { |n| "#{n}#{Time.now.to_i}#{rand(100000)}" }
 
     name { "Alice Pleasance Liddell #{uid}" }
     email { "#{uid}@example.com" }
     first_name 'Alice'
-    last_name 'Liddell'
+    last_name { "Liddell #{uid}" }
     image 'http://example.com/alice/image'
   end
 end
