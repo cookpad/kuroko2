57266cdccac720aafd8ed97139ebdbd5ba21041a
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-09-14 20:04:40 +0900

    Rake db:migrate

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,52 +13,55 @@
 
 ActiveRecord::Schema.define(version: 25) do
 
-  create_table "admin_assignments", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",           null: false, unsigned: true
-    t.integer  "job_definition_id", null: false, unsigned: true
+  create_table "admin_assignments", force: :cascade do |t|
+    t.integer  "user_id",           limit: 4, null: false
+    t.integer  "job_definition_id", limit: 4, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["user_id", "job_definition_id"], name: "user_id", unique: true, using: :btree
   end
 
-  create_table "executions", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+  add_index "admin_assignments", ["user_id", "job_definition_id"], name: "user_id", unique: true, using: :btree
+
+  create_table "executions", force: :cascade do |t|
     t.string   "uuid",                   limit: 36,                              null: false
-    t.integer  "job_definition_id",                                                           unsigned: true
-    t.integer  "job_definition_version",                                                      unsigned: true
-    t.integer  "job_instance_id",                                                             unsigned: true
-    t.integer  "token_id",                                                                    unsigned: true
+    t.integer  "job_definition_id",      limit: 4
+    t.integer  "job_definition_version", limit: 4
+    t.integer  "job_instance_id",        limit: 4
+    t.integer  "token_id",               limit: 4
     t.string   "queue",                  limit: 180,        default: "@default", null: false
     t.text     "shell",                  limit: 65535,                           null: false
     t.text     "context",                limit: 65535,                           null: false
-    t.integer  "pid",                                                                         unsigned: true
+    t.integer  "pid",                    limit: 4
     t.text     "output",                 limit: 4294967295
-    t.integer  "exit_status",            limit: 1,                                            unsigned: true
+    t.integer  "exit_status",            limit: 1
     t.integer  "term_signal",            limit: 1
     t.datetime "started_at"
     t.datetime "finished_at"
     t.datetime "mailed_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["job_definition_id", "token_id"], name: "job_definition_id", unique: true, using: :btree
-    t.index ["started_at"], name: "started_at", using: :btree
   end
 
-  create_table "job_definition_tags", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "job_definition_id", null: false, unsigned: true
-    t.integer  "tag_id",            null: false, unsigned: true
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.index ["job_definition_id", "tag_id"], name: "job_definition_id", unique: true, using: :btree
-    t.index ["tag_id"], name: "job_definition_tags_tag_id", using: :btree
+  add_index "executions", ["job_definition_id", "token_id"], name: "job_definition_id", unique: true, using: :btree
+  add_index "executions", ["started_at"], name: "started_at", using: :btree
+
+  create_table "job_definition_tags", force: :cascade do |t|
+    t.integer  "job_definition_id", limit: 4, null: false
+    t.integer  "tag_id",            limit: 4, null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
-  create_table "job_definitions", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "version",                               default: 0,     null: false, unsigned: true
+  add_index "job_definition_tags", ["job_definition_id", "tag_id"], name: "job_definition_id", unique: true, using: :btree
+  add_index "job_definition_tags", ["tag_id"], name: "job_definition_tags_tag_id", using: :btree
+
+  create_table "job_definitions", force: :cascade do |t|
+    t.integer  "version",                 limit: 4,     default: 0,     null: false
     t.string   "name",                    limit: 180,                   null: false
     t.text     "description",             limit: 65535,                 null: false
     t.text     "script",                  limit: 65535,                 null: false
     t.boolean  "suspended",                             default: false, null: false
-    t.integer  "prevent_multi",                         default: 1,     null: false, unsigned: true
+    t.integer  "prevent_multi",           limit: 4,     default: 1,     null: false
     t.boolean  "notify_cancellation",                   default: true,  null: false
     t.string   "hipchat_room",            limit: 180,   default: "",    null: false
     t.boolean  "hipchat_notify_finished",               default: true,  null: false
@@ -66,111 +70,122 @@ ActiveRecord::Schema.define(version: 25) do
     t.boolean  "api_allowed",                           default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["name"], name: "name", using: :btree
   end
 
-  create_table "job_instances", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "job_definition_id",                    unsigned: true
-    t.integer  "job_definition_version",               unsigned: true
+  add_index "job_definitions", ["name"], name: "name", using: :btree
+
+  create_table "job_instances", force: :cascade do |t|
+    t.integer  "job_definition_id",      limit: 4
+    t.integer  "job_definition_version", limit: 4
     t.text     "script",                 limit: 65535
     t.datetime "finished_at"
     t.datetime "canceled_at"
     t.datetime "error_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["finished_at", "canceled_at", "job_definition_id"], name: "finished_at", using: :btree
-    t.index ["job_definition_id"], name: "job_definition_id", using: :btree
   end
 
-  create_table "job_schedules", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "job_definition_id",             unsigned: true
+  add_index "job_instances", ["finished_at", "canceled_at", "job_definition_id"], name: "finished_at", using: :btree
+  add_index "job_instances", ["job_definition_id"], name: "job_definition_id", using: :btree
+
+  create_table "job_schedules", force: :cascade do |t|
+    t.integer  "job_definition_id", limit: 4
     t.string   "cron",              limit: 180
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["job_definition_id", "cron"], name: "job_definition_id", unique: true, using: :btree
   end
 
-  create_table "job_suspend_schedules", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "job_definition_id",                          unsigned: true
+  add_index "job_schedules", ["job_definition_id", "cron"], name: "job_definition_id", unique: true, using: :btree
+
+  create_table "job_suspend_schedules", force: :cascade do |t|
+    t.integer  "job_definition_id", limit: 4
     t.string   "cron",              limit: 180
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
-    t.index ["job_definition_id", "cron"], name: "job_definition_id", unique: true, using: :btree
   end
 
-  create_table "logs", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.integer  "job_instance_id",                    unsigned: true
+  add_index "job_suspend_schedules", ["job_definition_id", "cron"], name: "job_definition_id", unique: true, using: :btree
+
+  create_table "logs", force: :cascade do |t|
+    t.integer  "job_instance_id", limit: 4
     t.string   "level",           limit: 10
     t.text     "message",         limit: 4294967295
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["job_instance_id"], name: "job_instance_id", using: :btree
   end
 
-  create_table "memory_consumption_logs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "job_instance_id"
-    t.integer  "value",           null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["job_instance_id"], name: "index_memory_consumption_logs_on_job_instance_id", using: :btree
+  add_index "logs", ["job_instance_id"], name: "job_instance_id", using: :btree
+
+  create_table "memory_consumption_logs", force: :cascade do |t|
+    t.integer  "job_instance_id", limit: 4
+    t.integer  "value",           limit: 4, null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  create_table "memory_expectancies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "expected_value",    default: 0, null: false
-    t.integer  "job_definition_id"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.index ["job_definition_id"], name: "index_memory_expectancies_on_job_definition_id", using: :btree
+  add_index "memory_consumption_logs", ["job_instance_id"], name: "index_memory_consumption_logs_on_job_instance_id", using: :btree
+
+  create_table "memory_expectancies", force: :cascade do |t|
+    t.integer  "expected_value",    limit: 4, default: 0, null: false
+    t.integer  "job_definition_id", limit: 4
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
   end
 
-  create_table "process_signals", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+  add_index "memory_expectancies", ["job_definition_id"], name: "index_memory_expectancies_on_job_definition_id", using: :btree
+
+  create_table "process_signals", force: :cascade do |t|
     t.string   "hostname",   limit: 180,   default: "", null: false
-    t.integer  "pid",                                   null: false, unsigned: true
-    t.integer  "number",     limit: 1,     default: 15, null: false, unsigned: true
+    t.integer  "pid",        limit: 4,                  null: false
+    t.integer  "number",     limit: 1,     default: 15, null: false
     t.datetime "started_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "message",    limit: 65535
-    t.index ["hostname", "started_at"], name: "hostname_started_at", using: :btree
   end
 
-  create_table "stars", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",           null: false, unsigned: true
-    t.integer  "job_definition_id", null: false, unsigned: true
+  add_index "process_signals", ["hostname", "started_at"], name: "hostname_started_at", using: :btree
+
+  create_table "stars", force: :cascade do |t|
+    t.integer  "user_id",           limit: 4, null: false
+    t.integer  "job_definition_id", limit: 4, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["user_id", "job_definition_id"], name: "user_id", unique: true, using: :btree
   end
 
-  create_table "tags", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", comment: "2014-05-22 Hiromu Shioya" do |t|
+  add_index "stars", ["user_id", "job_definition_id"], name: "user_id", unique: true, using: :btree
+
+  create_table "tags", force: :cascade do |t|
     t.string   "name",       limit: 100, null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.index ["name"], name: "name", unique: true, using: :btree
   end
 
-  create_table "ticks", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+  add_index "tags", ["name"], name: "name", unique: true, using: :btree
+
+  create_table "ticks", force: :cascade do |t|
     t.datetime "at"
   end
 
-  create_table "tokens", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
+  create_table "tokens", force: :cascade do |t|
     t.string   "uuid",                   limit: 36,                  null: false
-    t.integer  "job_definition_id",                                               unsigned: true
-    t.integer  "job_definition_version",                                          unsigned: true
-    t.integer  "job_instance_id",                                                 unsigned: true
-    t.integer  "parent_id"
+    t.integer  "job_definition_id",      limit: 4
+    t.integer  "job_definition_version", limit: 4
+    t.integer  "job_instance_id",        limit: 4
+    t.integer  "parent_id",              limit: 4
     t.text     "script",                 limit: 65535,               null: false
     t.string   "path",                   limit: 180,   default: "/", null: false
-    t.integer  "status",                               default: 0,   null: false
+    t.integer  "status",                 limit: 4,     default: 0,   null: false
     t.text     "message",                limit: 65535,               null: false
     t.text     "context",                limit: 65535,               null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["parent_id"], name: "parent_id", using: :btree
-    t.index ["status"], name: "status", using: :btree
   end
 
-  create_table "users", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  add_index "tokens", ["parent_id"], name: "parent_id", using: :btree
+  add_index "tokens", ["status"], name: "status", using: :btree
+
+  create_table "users", force: :cascade do |t|
     t.string   "provider",     limit: 180, default: "google_oauth2", null: false
     t.string   "uid",          limit: 180,                           null: false
     t.string   "name",         limit: 180, default: "",              null: false
@@ -181,20 +196,22 @@ ActiveRecord::Schema.define(version: 25) do
     t.datetime "suspended_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["email"], name: "email", unique: true, using: :btree
-    t.index ["uid", "suspended_at"], name: "uid_2", using: :btree
-    t.index ["uid"], name: "uid", unique: true, using: :btree
   end
 
-  create_table "workers", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  add_index "users", ["email"], name: "email", unique: true, using: :btree
+  add_index "users", ["uid", "suspended_at"], name: "uid_2", using: :btree
+  add_index "users", ["uid"], name: "uid", unique: true, using: :btree
+
+  create_table "workers", force: :cascade do |t|
     t.string   "hostname",     limit: 180,                      null: false
     t.integer  "worker_id",    limit: 1,                        null: false
     t.string   "queue",        limit: 180, default: "@default", null: false
     t.boolean  "working",                  default: false,      null: false
-    t.integer  "execution_id",                                               unsigned: true
+    t.integer  "execution_id", limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["hostname", "worker_id"], name: "hostname", unique: true, using: :btree
   end
+
+  add_index "workers", ["hostname", "worker_id"], name: "hostname", unique: true, using: :btree
 
 end
