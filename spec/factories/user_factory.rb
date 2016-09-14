6f20216e843c1d6016bae5c20c113dbc01f953d9
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-09-14 10:12:31 +0900

    Revert "Add randomize to user factory"
    This reverts commit 2ccf40ea591a8590e6481a4bc3670b4a3ad5e104.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :user, class: Kuroko2::User do
-    sequence(:uid) { |n| "#{n}#{Time.now.to_i}#{rand(100000)}" }
+    sequence(:uid) { |n| "#{n}" }
 
     name { "Alice Pleasance Liddell #{uid}" }
     email { "#{uid}@example.com" }
     first_name 'Alice'
-    last_name { "Liddell #{uid}" }
+    last_name 'Liddell'
     image 'http://example.com/alice/image'
   end
 end
