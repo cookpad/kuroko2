4b3d74c4d1ced5e78e427da659894bfb9dfffa60
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-09-14 19:29:59 +0900

    Remove up to alignment process

--- db/migrate/021_create_memory_expectancies.rb
+++ db/migrate/021_create_memory_expectancies.rb
@@ -13,19 +13,5 @@ class CreateMemoryExpectancies < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-
-    reversible do |dir|
-      dir.up do
-        # The mysql server's timezone is JST, and AR's timezone is set as UTC
-        # Same as:
-        #   JobDefinition.find_each {|definition| definition.create_memory_expectancy! }
-        execute <<-SQL
-          INSERT INTO `memory_expectancies`
-            (`job_definition_id`, `created_at`, `updated_at`)
-            SELECT `id`, UTC_TIMESTAMP(), UTC_TIMESTAMP() FROM `job_definitions`
-          ;
-        SQL
-      end
-    end
   end
 end
