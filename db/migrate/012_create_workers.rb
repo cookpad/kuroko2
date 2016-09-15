c726a19447e3b7d27ebd5e4dac0e1cc82fbc983c
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-09-15 10:54:14 +0900

    Add options to workers table

--- db/migrate/012_create_workers.rb
+++ db/migrate/012_create_workers.rb
@@ -1,6 +1,6 @@
 class CreateWorkers < ActiveRecord::Migration
   def change
-    create_table "workers" do |t|
+    create_table "workers", options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC' do |t|
       t.string   "hostname",     limit: 180,                      null: false
       t.integer  "worker_id",    limit: 1,                        null: false
       t.string   "queue",        limit: 180, default: "@default", null: false
