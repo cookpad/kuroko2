760f15d864a3e7393d5e0e7193848af29fc89b9c
Eisuke Oishi <eisuke-oishi@cookpad.com>
2016-11-10 15:48:50 +0900

    Support webhook notification

---
+++ lib/autoload/kuroko2/workflow/notifier/webhook.rb
@@ -0,0 +1,176 @@
+module Kuroko2
+  module Workflow
+    module Notifier
+      class Webhook
+        attr_reader :message_builder
+
+        HASH_ALGORITHM = 'sha256'
+        HMAC_DIGEST    = OpenSSL::Digest.new(HASH_ALGORITHM)
+
+        def initialize(instance)
+          @instance   = instance
+          @definition = instance.job_definition
+          @message_builder = Workflow::Notifier::Concerns::ChatMessageBuilder.new(instance)
+          @secret_token = Kuroko2.config.notifiers.webhook.try!(:secret_token)
+        end
+
+        def notify_launch
+          if @definition.hipchat_notify_finished?
+            request(
+              build_body(
+                action: 'notify_launch',
+                level: 'INFO',
+                subject: message_builder.launched_text,
+                message: @instance.logs.select{ |log| log.level == 'INFO' }.last.try!(:message),
+              )
+            )
+          end
+        end
+
+        def notify_retrying
+          if @definition.hipchat_notify_finished?
+            request(
+              build_body(
+                action: 'notify_retrying',
+                level: 'INFO',
+                subject: message_builder.retrying_text,
+                message: @instance.logs.last(2).first.message,
+              )
+            )
+          end
+        end
+
+        def notify_skipping
+          if @definition.hipchat_notify_finished?
+            request(
+              build_body(
+                action: 'notify_skipping',
+                level: 'INFO',
+                subject: message_builder.skipping_text,
+                message: @instance.logs.last(2).first.message,
+              )
+            )
+          end
+        end
+
+        def notify_cancellation
+          if @definition.notify_cancellation || @definition.hipchat_notify_finished?
+            request(
+              build_body(
+                action: 'notify_cancellation',
+                level: 'WARNING',
+                subject: message_builder.failure_text,
+                message: @instance.logs.select{ |log| log.level == 'WARN' }.last.try!(:message),
+              )
+            )
+          end
+        end
+
+        def notify_failure
+          request(
+            build_body(
+              action: 'notify_failure',
+              level: 'FAILURE',
+              subject: message_builder.failure_text,
+              message: @instance.logs.last(2).first.message,
+            )
+          )
+        end
+
+        def notify_critical
+          request(
+            build_body(
+              action: 'notify_critical',
+              level: 'CRITICAL',
+              subject: message_builder.failure_text,
+              message: @instance.logs.last(2).first.message,
+            )
+          )
+        end
+
+        def notify_finished
+          if @definition.hipchat_notify_finished?
+            request(
+              build_body(
+                action: 'notify_finished',
+                level: 'SUCCESS',
+                subject: message_builder.finished_text,
+              )
+            )
+          end
+        end
+
+        def notify_long_elapsed_time
+          request(
+            build_body(
+              action: 'notify_long_elapsed_time',
+              level: 'WARNING',
+              subject: message_builder.long_elapsed_time_text,
+            )
+          )
+        end
+
+        private
+
+        def request(body)
+          # X-Kuroko2-Id: Unique ID for this delivery.
+          # X-Kuroko2-Signature:
+          # Content-Type: application/json
+
+          json = body.to_json
+
+          url = URI.parse(@definition.webhook_url)
+          conn = Faraday.new(url: "#{url.scheme}://#{url.host}") do |faraday|
+            faraday.port = url.port
+            faraday.adapter Faraday.default_adapter
+          end
+
+          response = conn.post do |req|
+            req.url(url.path)
+
+            req.headers['X-Kuroko2-Id'] = SecureRandom.uuid
+            if @secret_token.present?
+              req.headers['X-Kuroko2-Signature'] = "#{HASH_ALGORITHM}=#{OpenSSL::HMAC.hexdigest(HMAC_DIGEST, @secret_token, json)}"
+            end
+
+            req.headers['User-Agent']   = 'Kuroko2-Webhook'
+            req.headers['Content-Type'] = 'application/json'
+            req.body = json
+          end
+
+          unless response.success?
+            Kuroko2.logger.fatal("Failure sending webhook: status=#{response.status} body=#{response.body}")
+          end
+        end
+
+        def build_body(action:, level:, subject:, message: nil)
+          {
+            action: action,
+            level: level,
+            subject: subject,
+            message: message,
+            job_instance: {
+              url: message_builder.job_instance_path,
+              id: @instance.id,
+              script: @instance.script,
+              finished_at: @instance.finished_at.try!(:iso8601),
+              canceled_at: @instance.canceled_at.try!(:iso8601),
+              error_at: @instance.error_at.try!(:iso8601),
+              created_at: @instance.created_at.try!(:iso8601),
+            },
+            job_definition: {
+              url: Kuroko2::Engine.routes.url_helpers.job_definition_url(
+                @definition,
+                host: Kuroko2.config.url_host,
+                protocol: Kuroko2.config.url_scheme,
+              ),
+              id: @definition.id,
+              name: @definition.name,
+              description: @definition.description,
+            }
+          }
+        end
+      end
+    end
+  end
+end
---
+++ spec/workflow/notifier/webhook_spec.rb
@@ -0,0 +1,187 @@
+require 'rails_helper'
+
+module Kuroko2::Workflow
+  describe Notifier::Webhook do
+    let(:job_name) { 'My Job' }
+    let(:slack_channel) { '#dummy' }
+    let(:webhook_url) { 'https://localhost/my/hook_point' }
+    let(:instance) do
+      create(:job_definition_with_instances, name: job_name, webhook_url: webhook_url).
+        job_instances.first
+    end
+
+    let(:notifier) { Notifier::Webhook.new(instance) }
+    let(:response) { { body: 'ok' } }
+    let(:stub) do
+      stub_request(:post, webhook_url).and_return(response)
+    end
+
+    before do
+      instance.logs.info('start!')
+    end
+
+    describe '#notify_failure' do
+      it 'sends failure mesasge' do
+        stub.with { |req|
+          expect(req.headers['X-Kuroko2-Id']).to be_present
+          expect(req.headers['X-Kuroko2-Signature']).to be_present
+          expect(JSON.parse(req.body)).to include("action"=>"notify_failure")
+        }
+
+        notifier.notify_failure
+        expect(stub).to have_been_requested
+      end
+    end
+
+    describe '#notify_cancellation' do
+      before do
+        instance.logs.warn('warn')
+        instance.job_definition.notify_cancellation = true
+        instance.save!
+      end
+
+      it 'sends cancellation mesasge' do
+        stub.with { |req|
+          expect(JSON.parse(req.body)).to include("action"=>"notify_cancellation")
+        }
+
+        notifier.notify_cancellation
+        expect(stub).to have_been_requested
+      end
+    end
+
+    describe '#notify_critical' do
+      it 'sends critical mesasge' do
+        stub.with { |req|
+          expect(JSON.parse(req.body)).to include("action"=>"notify_critical")
+        }
+
+        notifier.notify_critical
+        expect(stub).to have_been_requested
+      end
+   end
+
+    describe '#notify_finished' do
+      it 'sends finished mesasge' do
+        stub.with { |req|
+          expect(JSON.parse(req.body)).to include("action"=>"notify_finished")
+        }
+
+        notifier.notify_finished
+        expect(stub).to have_been_requested
+      end
+    end
+
+    describe '#notify_retrying' do
+      context 'with notify_finished' do
+        before do
+          instance.job_definition.hipchat_notify_finished = true
+          instance.save!
+        end
+
+        it 'sends retrying mesasge' do
+          stub.with { |req|
+            expect(JSON.parse(req.body)).to include("action"=>"notify_retrying")
+          }
+
+          notifier.notify_retrying
+          expect(stub).to have_been_requested
+        end
+      end
+
+      context 'without notify_finished' do
+        before do
+          instance.job_definition.hipchat_notify_finished = false
+          instance.save!
+        end
+
+        it 'sends retrying mesasge' do
+          notifier.notify_retrying
+          expect(stub).not_to have_been_requested
+        end
+      end
+    end
+
+    describe '#notify_skipping' do
+      context 'with notify_finished' do
+        before do
+          instance.job_definition.hipchat_notify_finished = true
+          instance.save!
+        end
+
+        it 'sends skipping mesasge' do
+          stub.with { |req|
+            expect(JSON.parse(req.body)).to include("action"=>"notify_skipping")
+          }
+
+          notifier.notify_skipping
+          expect(stub).to have_been_requested
+        end
+      end
+
+      context 'without notify_finished' do
+        before do
+          instance.job_definition.hipchat_notify_finished = false
+          instance.save!
+        end
+
+        it 'sends skipping mesasge' do
+          notifier.notify_skipping
+          expect(stub).not_to have_been_requested
+        end
+      end
+    end
+
+    describe '#notify_launch' do
+      context 'with notify_finished' do
+        before do
+          instance.job_definition.hipchat_notify_finished = true
+          instance.save!
+        end
+
+        it 'sends launch mesasge' do
+          stub.with { |req|
+            expect(JSON.parse(req.body)).to include("action"=>"notify_launch")
+          }
+
+          notifier.notify_launch
+          expect(stub).to have_been_requested
+        end
+      end
+
+      context 'without notify_finished' do
+        before do
+          instance.job_definition.hipchat_notify_finished = false
+          instance.save!
+        end
+
+        it 'sends launch mesasge' do
+          notifier.notify_launch
+          expect(stub).not_to have_been_requested
+        end
+      end
+    end
+
+    describe '#notify_long_elapsed_time' do
+      it 'sends warning mesasge' do
+        stub.with { |req|
+          expect(JSON.parse(req.body)).to include("action"=>"notify_long_elapsed_time")
+        }
+
+        notifier.notify_long_elapsed_time
+        expect(stub).to have_been_requested
+      end
+    end
+
+    context 'with invalid response' do
+      let(:response) { { status: 500 } }
+
+      it 'logs to response' do
+        stub
+        expect(Kuroko2.logger).to receive(:fatal).with(/Failure sending webhook/)
+        notifier.notify_failure
+        expect(stub).to have_been_requested
+      end
+    end
+  end
+end
