c06cfd3ad583daf3f90379e8728a3076014cd20a
Kohei Suzuki <eagletmt@gmail.com>
2017-04-01 17:25:40 +0900

    Render instance logs by JavaScript to fix flicker
    Since instance logs are updated every 2 seconds, it's hard to select and
    copy logs. I'd like to append instance logs instead of replacing them.

--- app/controllers/kuroko2/logs_controller.rb
+++ app/controllers/kuroko2/logs_controller.rb
@@ -1,15 +1,40 @@
 class Kuroko2::LogsController < Kuroko2::ApplicationController
   def index
-    @definition = Kuroko2::JobDefinition.find(logs_params[:job_definition_id])
-    @instance   = Kuroko2::JobInstance.find(logs_params[:job_instance_id])
-    @logs       = @instance.logs.order(:id)
+    definition = Kuroko2::JobDefinition.find(logs_params[:job_definition_id])
+    instance   = Kuroko2::JobInstance.find(logs_params[:job_instance_id])
+    logs       = instance.logs.order(:id)
 
-    render layout: false
+    render json: {
+      reload: instance.working? && !instance.error?,
+      logs: logs.map { |log|
+        {
+          id: log.id,
+          level: log.level,
+          class_for_label: class_for_label(log.level),
+          created_at: log.created_at,
+          message_html: Rinku.auto_link(ERB::Util.h(log.message), :urls),
+        }
+      },
+    }
   end
 
   private
 
   def logs_params
     params.permit(:job_definition_id, :job_instance_id)
+  end
+
+  def class_for_label(level)
+    modifier = case level
+               when 'INFO'
+                 'info'
+               when 'ERROR'
+                 'danger'
+               when 'WARN'
+                 'warning'
+               else
+                 'default'
+               end
+    "label-#{modifier}"
   end
 end
