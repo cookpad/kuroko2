43e51f2d8ad8375ef94412d6dd05b1bb1cd398e5
Hiroyuki Inoue <mammymax@gmail.com>
2017-06-26 12:56:04 -0700

    Use MutationObserver to oberve log for work instance_linker.js correctly

--- app/assets/javascripts/kuroko2/instance_linker.js
+++ app/assets/javascripts/kuroko2/instance_linker.js
@@ -1,10 +1,22 @@
 jQuery(function ($) {
-  $('td.log').each(function(){
-    var logText = $(this).html();
-    $(this).html(
-      logText.replace(/instance#(\d+)\/(\d+)(?:\s+|\.)/, function(match, jobId, instanceId){
-	return '<a href="/definitions/' + jobId + '/instances/' + instanceId+ '">' + match + '</a>';
-      })
-    );
+  var logParent = document.querySelector('#logs tbody');
+  var observer = new MutationObserver(function(mutations) {
+    if (mutations.some(x =>
+      x.addedNodes
+        && x.addedNodes instanceof NodeList
+        && x.addedNodes.length > 0
+        && x.type == 'childList'
+      )
+    ) {
+      $('td.log').each(function() {
+        var logText = $(this).html();
+        $(this).html(
+          logText.replace(/instance#(\d+)\/(\d+)(?:\s+|\.|$)/, function(match, jobId, instanceId) {
+            return '<a href="/definitions/' + jobId + '/instances/' + instanceId+ '">' + match + '</a>';
+          })
+        );
+      });
+    }
   });
+  observer.observe(logParent, {childList: true, substree: true});
 });
