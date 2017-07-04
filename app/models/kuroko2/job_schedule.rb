ba4836f3a466bc2faa44d997a6079259e656da7c
Eisuke Oishi <eisuke-oishi@cookpad.com>
2017-07-04 17:30:38 +0900

    Use `public_send`

--- app/models/kuroko2/job_schedule.rb
+++ app/models/kuroko2/job_schedule.rb
@@ -65,13 +65,13 @@ class Kuroko2::JobSchedule < Kuroko2::ApplicationRecord
 
     launch_schedule = Chrono::Schedule.new(cron)
     schedule = CHRONO_SCHEDULE_METHODS.each_with_object({}) do |method, h|
-      h[method] = launch_schedule.send(method)
+      h[method] = launch_schedule.public_send(method)
     end
 
     job_definition.job_suspend_schedules.each do |suspend_schedule_model|
       suspend_schedule = Chrono::Schedule.new(suspend_schedule_model.cron)
       CHRONO_SCHEDULE_METHODS.each do |method|
-        schedule[method] -= suspend_schedule.send(method)
+        schedule[method] -= suspend_schedule.public_send(method)
       end
 
       # https://linux.die.net/man/5/crontab
