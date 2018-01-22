77e281fdf5018ab8b818da976c1fe938bb8198e9
takonomura <takonomura@users.noreply.github.com>
2018-01-22 13:29:10 +0900

    Add spec for workers controller

--- spec/controllers/workers_controller_spec.rb
+++ spec/controllers/workers_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Kuroko2::WorkersController, :type => :controller do
   routes { Kuroko2::Engine.routes }
+
+  before { sign_in }
+
+  let(:worker) { create(:worker) }
+
+  describe '#update' do
+    subject! { patch :update, params: { id: worker.id, suspended: true } }
+
+    it do
+      expect(response).to redirect_to(workers_path)
+
+      expect(assigns(:worker).suspended).to be_truthy
+    end
+  end
 end
