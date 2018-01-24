88fcc64f199e99a3a2821e4e707aedd6aa233966
takonomura <takonomura@users.noreply.github.com>
2018-01-24 11:17:14 +0900

    Change ActiveRecord::Migration version

--- db/migrate/031_add_suspended_to_workers.rb
+++ db/migrate/031_add_suspended_to_workers.rb
@@ -1,4 +1,4 @@
-class AddSuspendedToWorkers < ActiveRecord::Migration[5.1]
+class AddSuspendedToWorkers < ActiveRecord::Migration[5.0]
   def change
     add_column :workers, :suspended, :boolean, default: false, null: false, after: :execution_id
   end
